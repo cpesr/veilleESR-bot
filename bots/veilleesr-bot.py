efd1005c25dea8779b06bd984099edc5dfb517b0
Julien Gossa <julien.gossa@gmail.com>
2020-10-14 11:00:12 +0200

    Ajout autotweet

--- bots/veilleesr-bot.py
+++ bots/veilleesr-bot.py
@@ -5,6 +5,7 @@ import tweepy
 import logging
 from config import create_api
 import json
+import time
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
@@ -35,12 +36,34 @@ class FavRetweetListener(tweepy.StreamListener):
 
     def on_error(self, status):
         logger.error(status)
+        
+class AutoTweet:
+    def __init__(self, api, urlfilename):
+        self.api = api
+        urlfile = open("url-list.txt","r")
+        self.urls = urlfile.readlines()
+        
+    def tweet(self, delay):
+        i = 0
+        while True:
+            logger.info(f"Processing url {self.urls[i]}")
+            try:
+                self.api.update_status(self.urls[i])
+            except Exception as e:
+                logger.error("Error on autotweet", exc_info=True)               
+            time.sleep(delay)
+            i = (i+1) % len(self.urls)
 
-def main(keywords):
+def main():
     api = create_api()
     tweets_listener = FavRetweetListener(api)
     stream = tweepy.Stream(api.auth, tweets_listener)
-    stream.filter(track=keywords, languages=["fr","en"])
+    stream.filter(track=["#VeilleESR", "#DataESR"], languages=["fr","en"], is_async = True)
+    
+    autotweet = AutoTweet(api, "url-list.txt")
+    autotweet.tweet(86400)
+    
+    
 
 if __name__ == "__main__":
-    main(["#VeilleESR", "#DataESR"])
+    main()
