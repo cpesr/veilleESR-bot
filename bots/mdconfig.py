0c9fb5fd6eec0f7495aafb24b190824b7b24cece
juliengossa <julien.gossa@gmail.com>
2021-09-27 22:03:13 +0200

    Ajout d'un fonction de tweet en batch

--- bots/mdconfig.py
+++ bots/mdconfig.py
@@ -27,12 +27,19 @@ def get_mdconfig(url):
 
     return({'config':config, 'tweets':tweets, 'datatweets':datatweets})
 
+def md2img(line):
+    end = line.find(".png")
+    if end == -1: return None
+    start = line.find('src="') + 5
+    if start == -1: start = line.find("(") + 2
+    return( line[start:end+4] )
+
 def get_datamd(url):
     pm = urllib3.PoolManager()
     data = pm.request("GET", url, preload_content=False)
     datamd = data.read().decode('utf-8').splitlines()
 
-    twtexte = "#DataESR"
+    twtexte = "[#DataESR]"
     twalt = ""
     twurl = ""
 
@@ -41,14 +48,14 @@ def get_datamd(url):
         dm = dm.strip(" ")
         if len(dm) > 0:
             if dm[0:9] == "- twtexte": twtexte = dm[10:].strip(" ")
-            if dm[0:7] == "- twalt": twalt = dm[8:].strip(" ")
-            if dm[0:7] == "- twurl": twurl = dm[8:].strip(" <>")
-            if dm[0] == "#": dttext = twtexte + '\n' + dm.lstrip('# ')
-            if dm[0] == "!":
-                dtimgurl = dm.lstrip("![](").rstrip(")<!- ->")
-                dtimgurl = path.dirname(url) + "/" + dtimgurl
-
-                datatweets.append({'text':dttext,'imgurl':dtimgurl, 'alt':twalt, 'url':twurl})
+            elif dm[0:7] == "- twalt": twalt = dm[8:].strip(" ")
+            elif dm[0:7] == "- twurl": twurl = dm[8:].strip(" <>")
+            elif dm[0] == "#": dttext = twtexte + '.\n\n\U0001F4CA \U0001F4C9 ' + dm.lstrip('# ') + '.'
+            else:
+                dtimgurl = md2img(dm)
+                if dtimgurl is not None:
+                    dtimgurl = path.dirname(url) + "/" + dtimgurl
+                    datatweets.append({'text':dttext, 'imgurl':dtimgurl, 'alt':twalt, 'url':twurl})
 
     return(datatweets)
 
